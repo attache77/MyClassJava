8a64cb532330d252b4aa1e4c3784abdc4cd15c71
Attache Hardy <attachehardy@gmail.com>
2020-11-18 20:02:01 -0500

    First commit Uploading All Files

---
+++ MyClass/src/MyClass.java
@@ -0,0 +1,47 @@
+
+public class MyClass {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	String fullName = "Thomas Mattocks";
+	System.out.println(fullName);
+	
+	//int x = 100 + 50;
+	//System.out.println(x);
+	
+	//int sum1 = 100 + 50;
+	//int sum2 = sum1 + 250;
+	//int sum3 = sum2 + sum2;
+	//System.out.println(sum1);
+	//System.out.println(sum2);
+	//System.out.println(sum3);
+	
+	//int x = 5;
+	//int y = 10;
+	//System.out.println(x + y);
+	
+	//int x = 20;
+	//--x;
+	//System.out.println(x);
+	
+	//int x = 10;
+	//ystem.out.println(x);
+
+	////int x = 10;
+	//x +=20;
+	//System.out.println(x);
+	
+	int x = 5;
+	int y = 10;
+	System.out.println(x != y);
+	
+	
+
+
+	
+
+
+
+	}
+
+}
